9371925a5ad81638123ed381fe3ad9c1efe3ffdf
Benoit Lemaire <blemaire@walmartlabs.com>
2017-10-05 13:09:07 -0700

    Use FlatList instead of ListView

--- index.ios.js
+++ index.ios.js
@@ -8,7 +8,7 @@ import {
   StyleSheet,
   Text,
   View,
-  ListView,
+  FlatList,
   Image,
   TouchableHighlight
 } from 'react-native'
@@ -17,76 +17,74 @@ import { NavigationApi } from 'react-native-ernnavigation-api'
 
 export default class MovieListMiniApp extends Component {
 
-  constructor () {
-       super()
-       const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+  _keyExtractor = (item, index) => item.title;
 
-       let topMovies = []
-       MoviesApi.requests().getTopRatedMovies().then((movies) => {
-         console.log(`Top movies fetched ${movies}`)
-         if (movies) {
-           this.setState(previousState => {
-             return {dataSource: ds.cloneWithRows(movies)}
-           })
-         }
-       }).catch(error => {
-         console.log(`Error: ${error}`)
-         topMovies = [{
-           title: 'Titanic',
-           releaseYear: 1997,
-           ratings: '4.5',
-           imageUrl: `https://images-na.ssl-images-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SY1000_CR0,0,671,1000_AL_.jpg`,
-           description: 'Titanic'
-         }, {
-           title: 'Avatar',
-           releaseYear: 2009,
-           ratings: '4.0',
-           imageUrl: `https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_.jpg`,
-           description: 'Avatar'
-         }
-         ]
+constructor () {
+  super()
 
-         this.setState(previousState => {
-           return {dataSource: ds.cloneWithRows(topMovies)}
-         })
+  MoviesApi.requests().getTopRatedMovies().then((movies) => {
+    if (movies) {
+      this.setState(previousState => {
+        return {movies}
+      })
+    }
+  }).catch(error => {
+    let movies = [{
+      title: 'Titanic',
+      releaseYear: 1997,
+      ratings: '4.5',
+      imageUrl: 'http://bit.ly/2hnU8mq',
+      description: 'Titanic'
+    }, {
+      title: 'Avatar',
+      releaseYear: 2009,
+      ratings: '4.0',
+      imageUrl: 'http://bit.ly/2xAX0Cv',
+      description: 'Avatar'
+    }]
 
-       })
+    this.setState(previousState => {
+      return {movies}
+    })
+  })
 
-       this.state = {
-         dataSource: ds.cloneWithRows(topMovies),
-       }
-     }
+  this.state = {
+    movies: []
+  }
+}
 
-     render () {
-        return (
-          <ListView style={styles.container} dataSource={this.state.dataSource}
-                    renderRow={(movie) =>
-                    <TouchableHighlight onPress={() => this._onPressRow(movie)} underlayColor="grey">
-                      <View style={styles.row} onPress={() => this._onPressRow(movie)}>
-                        <Image
-                          style={styles.icon}
-                          source={{
-                            uri: movie.imageUrl ? movie.imageUrl : 'http://image10.bizrate-images.com/resize?sq=60&uid=2216744464?odnHeight=100&odnWidth=100&odnBg=FFFFFF'
-                          }}
-                        />
-                        <View style={styles.row2}>
-                          <Text style={styles.title}>{movie.title}</Text>
-                          <Text style={styles.subtitle}>{movie.releaseYear}</Text>
-                        </View>
-                      </View>
-                    </TouchableHighlight>
-                    }
-                    renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator}/>}
+
+render () {
+  return (
+    <FlatList
+      style={styles.container}
+      data={this.state.movies}
+      keyExtractor={this._keyExtractor}
+      renderItem={({item}) =>
+      <TouchableHighlight onPress={() => this._onPressRow(item)} underlayColor="gray">
+        <View style={styles.row} onPress={() => this._onPressRow(item)}>
+          <Image
+            style={styles.icon}
+            source={{
+              uri: item.imageUrl ? item.imageUrl : 'http://bit.ly/2yz3AYe'
+            }}
           />
-        )
-      }
+          <View style={styles.row2}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.subtitle}>{item.releaseYear}</Text>
+          </View>
+        </View>
+        </TouchableHighlight>}
+    />
+  )
+}
 
-      _onPressRow (movie) {
-        movie.isSelect = !movie.isSelect
-        NavigationApi.requests().navigate('MovieDetailsMiniApp', {'initialPayload': JSON.stringify(movie)}).catch(() => {
-          console.log("Navigation failed.");
-        })
-      }
+  _onPressRow (movie) {
+     movie.isSelect = !movie.isSelect
+     NavigationApi.requests().navigate('MovieDetailsMiniApp', {'initialPayload': JSON.stringify(movie)}).catch(() => {
+       console.log("Navigation failed.");
+     })
+   }
 }
 
 const styles = StyleSheet.create({
@@ -96,7 +94,6 @@ const styles = StyleSheet.create({
     padding: 5,
     backgroundColor: 'black'
   },
-
   row: {
     flex: 1,
     flexDirection: 'row',
@@ -111,13 +108,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 12
   },
-  listview: {
-    flex: 1,
-    padding: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
   title: {
     fontSize: 20,
   },
@@ -125,12 +115,6 @@ const styles = StyleSheet.create({
     paddingTop: 5,
     flex: 1,
     fontSize: 12
-  },
-  separator: {
-    flex: 1,
-    height: 0,
-    paddingTop: 2,
-    paddingBottom: 2
   },
   icon: {
     width: 50,
